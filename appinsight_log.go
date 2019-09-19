cae7a78fd7f914b8701d31d04102df89c5f27573
lekojson <jesper.j.tornros@gmail.com>
2019-09-19 10:56:08 +0200

    add appinsights logger

---
+++ appinsight_log.go
@@ -0,0 +1,50 @@
+package log
+
+import (
+	"fmt"
+
+	"github.com/microsoft/ApplicationInsights-Go/appinsights"
+	"github.com/microsoft/ApplicationInsights-Go/appinsights/contracts"
+)
+
+type AppInsightsLogger struct {
+	c     appinsights.TelemetryClient
+	props map[string]string
+}
+
+func (l *AppInsightsLogger) Logf(level Level, format string, v ...interface{}) {
+	ailevel := contracts.Verbose
+	switch level {
+	case LevelInfo:
+		ailevel = contracts.Information
+	case LevelWarning:
+		ailevel = contracts.Warning
+	case LevelError:
+		ailevel = contracts.Error
+	case LevelCritical:
+		ailevel = contracts.Critical
+	}
+
+	trace := appinsights.NewTraceTelemetry(fmt.Sprintf(format, v...), ailevel)
+	if l.props != nil {
+		for k, v := range l.props {
+			trace.Properties[k] = v
+		}
+	}
+
+	l.c.Track(trace)
+}
+
+func (l *AppInsightsLogger) Push() error { return nil }
+
+type AppInsightsConfig struct {
+	TelemetryClient appinsights.TelemetryClient
+	Properties      map[string]string
+}
+
+func NewAppInsightsLogger(config *AppInsightsConfig) *AppInsightsLogger {
+	return &AppInsightsLogger{
+		c:     config.TelemetryClient,
+		props: config.Properties,
+	}
+}
